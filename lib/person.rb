b49c8be8fdfc57ff4059106b6e995f21caa2a722
shaywacker <shay2442@yahoo.com>
2021-12-14 10:15:49 -0600

    commit tree 36a455190e22d06a3b72303f1340698eef9da486

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
+require "pry"
 class Person
-  # your code here
-end
+    def initialize(attributes)
+      attributes.each do |key, value|
+        # create a getter and setter by calling the attr_accessor method
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+      end
+    end
+  end
+
+
+
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+shay = {name: "Shay", hair_color: "Blonde", eye_color: "Blue"}
+
+# binding.pry
